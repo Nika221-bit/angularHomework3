45b42aafddfa783a565511c7edd5b2c32cf15aa0
Nikoloz_22 <K0K0L0z1922@gmail.com>
2026-03-19 15:28:40 +0400

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('angularHomework3');
+
+  email: string = "";
+
+  welcome() {
+    alert("მოგესალმები");
+  }
 }
+
